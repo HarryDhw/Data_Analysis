63004d0a42619020f3dbab49f926e16daddf3eca
Harry <greatd24@icloud.com>
2022-07-19 14:38:39 +0800

    csv if finished

--- sitka_highs.py
+++ sitka_highs.py
@@ -2,7 +2,9 @@ import csv
 import matplotlib.pyplot as plt
 from datetime import datetime
 
-filename = 'sitka_weather_07-2018_simple.csv'
+from traitlets import Int
+
+filename = 'sitka_weather_2018_simple.csv'
 with open(filename) as f:
     reader = csv.reader(f)  #csv.reader(f)读取文件并赋值给变量reader
     header_row = next(reader)   #next(reader)读取文件第一行
@@ -14,20 +16,39 @@ with open(filename) as f:
 
     #提取并读取数据
     #从文件中获取最高温度和日期
-    highs, dates = [], []
-    for row in reader:  #读取文件的每一行
-        current_date = datetime.strptime(row[2], '%Y-%m-%d')#日期转化
-        dates.append(current_date)
-        high = int(row[5])  #上边已知最高温度是索引5,并用int转换为整数
-        highs.append(high)
+    # highs, dates, lows = [], [], []
+    # for row in reader:  #读取文件的每一行
+    #     current_date = datetime.strptime(row[2], '%Y-%m-%d')#日期转化
+    #     dates.append(current_date)
+    #     high = int(row[5])  #上边已知最高温度是索引5,并用int转换为整数
+    #     highs.append(high)
+    #     low = int(row[6])
+    #     lows.append(low)
+
+    #如果数据有错误怎么办？
+    dates, highs, lows = [], [], []
+    for row in reader:
+        current_date = datetime.strptime(row[2], '%Y-%m-%d')
+        try:   #对于每一行都尝试从中提取日期、最高和最低温度
+            high = int(row[4])
+            low = int(row[5])
+        except ValueError:  #只要缺失其中一个数据就会引发ValueError异常
+            print(f'Missing data for {current_date}') #我们让它打印一条消息指出缺失数据的日期
+        else: #没有错误后将会执行下面代码
+            dates.append(current_date)
+            highs.append(high)
+            lows.append(low)
 
 #根据最高温度绘制图形
 plt.style.use('Solarize_Light2')
 fig, ax = plt.subplots()
-ax.plot(dates, highs, c='red')
+ax.plot(dates, highs, c='red', alpha=0.5)#alpha指定颜色的透明度
+ax.plot(dates, lows, c='blue', alpha=0.5)
+#fill_between给图表区域着色，两个y之间的区域
+ax.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
 #设置图形格式
-ax.set_title('2018年7月每日最高温度', fontsize=24)
+ax.set_title('2018年每日最高和最低温度\n美国加利福尼亚死亡谷', fontsize=24)
 ax.set_xlabel('日期', fontsize=16)
 fig.autofmt_xdate() #把日期绘制倾斜以免彼此重复
 ax.set_ylabel('温度(F)', fontsize=16)
--- test.py
+++ test.py
@@ -1,22 +1,25 @@
 # import matplotlib.pyplot as plt
 # print(plt.style.available)
 
-from datetime import datetime
+# from datetime import datetime
 
-first_date = datetime.strptime('2018-07-01', '%Y-%m-%d')
-print(first_date)
+# first_date = datetime.strptime('2018-07-01', '%Y-%m-%d')
+# print(first_date)
 
-'''
-模块datetime 中设置日期和时间格式的实参
-%A 星期几  如Monday
-%B 月份名   如January
-%m 用数表示的月份   如(01~12)
-%d 用数表示的月份中的一天   如(01~31)
-%Y 四位的年份   如2019
-%y 两位的年份   如19
-%H 24小时制的小时数 (00~23)
-%I 12小时制的小时数 (01~12)
-%p am或pm
-%M 分钟数(00~59)
-%S 秒数(00~59)
-'''
+# '''
+# 模块datetime 中设置日期和时间格式的实参
+# %A 星期几  如Monday
+# %B 月份名   如January
+# %m 用数表示的月份   如(01~12)
+# %d 用数表示的月份中的一天   如(01~31)
+# %Y 四位的年份   如2019
+# %y 两位的年份   如19
+# %H 24小时制的小时数 (00~23)
+# %I 12小时制的小时数 (01~12)
+# %p am或pm
+# %M 分钟数(00~59)
+# %S 秒数(00~59)
+# '''
+
+for index, item in enumerate(['harry', 'yoyo', 'jimi']):
+    print(index, item)
